51498de63885abd942b6b2c7574b2ef8d0847708
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-09 23:51:26 +0900

    #4 feature: 하단 바 컴포넌트 구현 - componets/TabBar.tsx 하단 바 컴포넌트 구현하였습니다. - app/index.tsx 스크롤시 상단 바와 하단 바 고정되게 코드 수정하였습니다.
    Resolves: #4
    See also: None

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,21 @@
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
 import TopBar from '../components/TopBar';
 import SearchBar from '../components/SearchBar';
+import TabBar from '../components/TabBar';
 
 export default function Home() {
-  return (
-    <SafeAreaView className="flex-1 bg-white" edges={['top']}>
-      <TopBar />
-      <SearchBar />
-      <View className="flex-1 px-5" />
-    </SafeAreaView>
-  );
+    const [activeTab, setActiveTab] = useState('home');
+
+    return (
+        <SafeAreaView className="flex-1 bg-white" edges={['top', 'bottom']}>
+            <TopBar />
+            <SearchBar />
+            <ScrollView className="flex-1 px-5" scrollEventThrottle={16}>
+                {/* 콘텐츠가 여기에 들어갑니다 */}
+            </ScrollView>
+            <TabBar activeTab={activeTab} onTabPress={setActiveTab} />
+        </SafeAreaView>
+    );
 }
