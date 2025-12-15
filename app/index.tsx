c1ea196913a2240cc091eb10d206dba1cec45e35
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-15 19:34:32 +0900

    #8 feature: 상품 리스트 컴포넌트 구현 - componets/Product.tsx 상품을 화면에 표시하는 기본 상품 컴포넌트 구현하였습니다.
    Resolves: #8
    See also: None

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import TopBar from '../components/TopBar';
 import SearchBar from '../components/SearchBar';
 import CategoryBar from '../components/CategoryBar';
+import ProductList from '../components/ProductList';
 import TabBar from '../components/TabBar';
 
 export default function Home() {
@@ -11,12 +12,12 @@ export default function Home() {
     const [activeCategory, setActiveCategory] = useState('all');
 
     return (
-        <SafeAreaView className="flex-1 bg-white" edges={['top', 'bottom']}>
+        <SafeAreaView className="flex-1 bg-gray-50" edges={['top', 'bottom']}>
             <TopBar />
             <SearchBar />
             <CategoryBar activeCategory={activeCategory} onCategoryPress={setActiveCategory} />
-            <ScrollView className="flex-1 px-5" scrollEventThrottle={16}>
-                {/* 메인 콘텐츠 */}
+            <ScrollView className="flex-1 px-5" scrollEventThrottle={16} showsVerticalScrollIndicator={false}>
+                <ProductList />
             </ScrollView>
             <TabBar activeTab={activeTab} onTabPress={setActiveTab} />
         </SafeAreaView>
